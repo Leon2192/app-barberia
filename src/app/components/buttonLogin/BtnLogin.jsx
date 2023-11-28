a3c735aa6cac088eaa120e460f6aa513f1c03ac8
leon_2192 <leon.strokes@gmail.com>
2023-11-28 18:02:23 -0300

    fix context

--- src/app/components/buttonLogin/BtnLogin.jsx
+++ src/app/components/buttonLogin/BtnLogin.jsx
@@ -1,12 +1,10 @@
-//"use client"
+"use client"
 import { AuthContext, UserAuth } from "@/app/context/AuthContext";
 import { Button } from "@nextui-org/button";
 import { useContext } from "react";
 import { Toaster } from 'react-hot-toast';
-//import { UserAuth } from '../../context/AuthContext'
 
 export default function BtnLogin() {
-    //const { handleLogin, userExist, handleLogout } = UserAuth()
     const { handleLogin, handleLogout, userExist } = useContext(AuthContext)
 
     return (
